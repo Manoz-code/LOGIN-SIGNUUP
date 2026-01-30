9a78bd1412b819100f007724010873f6dee618b1
manoz <kunwarmanoz434@gmail.com>
2026-01-30 03:31:04 +0300

    Added userlogin

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 
 // REGISTER USER
@@ -16,4 +17,22 @@ export const registerUser = asyncHandler(async(req,res)=>{
     const newUser = await User.create({name,email,password : hashedPassword});
     res.status(201).json({message : "user created", newUser});
 
+})
+
+// LOGIN USER
+export const loginUser = asyncHandler(async(req,res)=>{
+    const {email,password}= req.body;
+    const user = await User.findOne({email});
+            if (!user) throw new Error("User doesn't exist");
+            //verify password 
+            const isMatch = bcrypt.compare(password, user.password);
+            if(!isMatch) throw new Error("Wrong Incredeint");
+
+            const token = jwt.sign({id: user._id,name:user.name}, process.env.TOKEN,{expiresIn:"1h"});
+            res.status(200).json({
+                message : "LOGIN SUCESSFULLY",
+                token
+            })
+
+    
 })
